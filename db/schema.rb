c1b7e25e08189bc08eaed8b9ce7fd304fd0a8a4a
Edward Schults <edward.schults@gmail.com>
2015-02-14 15:23:09 +0100

    updated order.price

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150214140906) do
+ActiveRecord::Schema.define(version: 20150214142221) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 20150214140906) do
     t.datetime "pickup_date"
     t.datetime "delivery_date"
     t.boolean  "paid"
-    t.integer  "price_cents"
+    t.float    "price"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
