6a7b5cd9e86647fd28f06be7d6e682074a1f0925
Edward Schults <edward.schults@gmail.com>
2015-02-16 12:49:25 +0100

    added restriction on datepickers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :street, :city, :zipcode, :address_details)
+    params.require(:user).permit(:first_name, :last_name, :email, :phone, :street, :city, :zipcode, :address_details)
   end
 
   def set_user
