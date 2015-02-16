14d33e9eb6da59a8ec21a36fd2b6a1ab8d0270c8
Edward Schults <edward.schults@gmail.com>
2015-02-16 09:44:50 +0100

    added delivery_time and pickup_time to Order

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150214142221) do
+ActiveRecord::Schema.define(version: 20150216084416) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,8 @@ ActiveRecord::Schema.define(version: 20150214142221) do
     t.float    "price"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.datetime "delivery_time"
+    t.datetime "pickup_time"
   end
 
   add_index "orders", ["user_id"], name: "index_orders_on_user_id", using: :btree
