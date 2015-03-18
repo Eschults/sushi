21af0bca200de2ed4dbe364620cedf36ade3189f
Florian Boret <florianboret@gmail.com>
2015-03-18 17:56:33 +0100

    s dans adresse de production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: 'http://www.hipcleaner.com' }
+  config.action_mailer.default_url_options = { host: 'https://www.hipcleaner.com' }
 
   # Settings specified here will take precedence over those in config/application.rb.
 
