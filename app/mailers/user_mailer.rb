01bda275c2dca610ec7f5594671b62b9fa0cdc35
Florian Boret <florianboret@gmail.com>
2015-03-20 18:04:45 +0100

    desgin button

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-  default from: "contact@hipcleaner.com"
+  default from: '"HipCleaner" <contact@hipcleaner.com>'
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
