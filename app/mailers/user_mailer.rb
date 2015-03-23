76031fa2b68e788fadc7a578cb98228a40da2217
Florian Boret <florianboret@gmail.com>
2015-03-23 17:07:20 +0100

    mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -9,7 +9,7 @@ class UserMailer < ActionMailer::Base
   def welcome(user)
     @user = user
     mail(to: @user.email,
-         bcc: "contact@hipcleaner.com",
+         bcc: "antoinegarcia@hipcleaner.com",
          subject: "Bienvenue chez HipCleaner !")
   end
 end
